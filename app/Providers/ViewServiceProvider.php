d4ec738c269ab1d1aed503b7b741134e43b2ef1b
Escral <F9v6K4Fu42>
2019-07-19 09:02:16 +0300

    Metadata

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -42,7 +42,7 @@ class ViewServiceProvider extends ServiceProvider
             $data = $view->getData();
 
             if (! isset($data['meta']) or ! is_iterable($data['meta']) or ! isset($data['meta']['title'])) {
-                $view->with('meta', $indexPage ? optional($indexPage->metadata)->toArray() : [
+                $view->with('meta', $indexPage and $indexPage->metadata ? $indexPage->metadata->toArray() : [
                     'title' => config('app.name')
                 ]);
             }

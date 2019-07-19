3932cb2708dd911787fcdaa7018e8dda6efb73ca
Escral <F9v6K4Fu42>
2019-07-19 12:43:06 +0300

    Metadata

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,110 +9,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        $canonical = false;
-
-        $services = [
-            [
-                'icon' => 'decay',
-                'img'  => '2.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => true,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'tooth',
-                'img'  => '2.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => true,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'tooth1',
-                'img'  => '3.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => true,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'brackets',
-                'img'  => '4.jpg',
-                'list' => false,
-                'name' => 'Профессиональная диагностика',
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'dentures',
-                'img'  => '5.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => false,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'tooth2',
-                'img'  => '6.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => false,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'tooth4',
-                'img'  => '7.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => false,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-            [
-                'icon' => 'tooth3',
-                'img'  => '8.jpg',
-                'name' => 'Профессиональная диагностика',
-                'list' => false,
-                'text' => 'Консультации всех наших специалистов в области терапии, ортопедии, ортодонтологии'
-            ],
-        ];
-
-        $advantages = [
-            [
-                'icon'  => 'tooth-whitening',
-                'count' => '01',
-                'name'  => 'Материалы и оборудование',
-                'text'  => 'Десятилетний опыт создания концепции
-                        индивидуального лечения для сохранения
-                        красоты и здоровья ваших зубов'
-            ],
-            [
-                'icon'  => 'doctor',
-                'count' => '02',
-                'name'  => 'Материалы',
-                'text'  => 'Десятилетний опыт создания концепции
-                        индивидуального лечения для сохранения
-                        красоты и здоровья ваших зубов'
-            ],
-            [
-                'icon'  => 'cardiogram',
-                'count' => '03',
-                'name'  => 'Материалы',
-                'text'  => 'Десятилетний опыт создания концепции
-                        индивидуального в'
-            ],
-            [
-                'classMod' => 'advantage--lg',
-                'icon'     => 'technology',
-                'count'    => '04',
-                'name'     => 'Материалы',
-                'text'     => 'Десятилетний опыт создания концепции
-                        индивидуального в'
-            ],
-            [
-                'classMod' => 'advantage--lg',
-                'icon'     => 'respect',
-                'count'    => '05',
-                'name'     => 'Материалы',
-                'text'     => 'Десятилетний опыт создания концепции
-                        индивидуального в'
-            ],
-        ];
-
-        return view('pages.index', compact('services', 'advantages', 'canonical'));
+        return view('pages.index');
     }
 
     public function show($page)
@@ -123,17 +20,9 @@ class PageController extends Controller
             'page'        => $page,
             'breadcrumbs' => Breadcrumbs::page($page),
             'pageTitle'   => $page->title,
-            'meta'        => $page->metadata->toArray(),
+            'meta'        => array_merge([
+                'title'       => $page->title,
+            ], optional($page->metadata)->toArray() ?? []),
         ]);
-    }
-
-    public function blog()
-    {
-        return view('pages.blog');
-    }
-
-    public function doctor()
-    {
-        return view('pages.doctor');
     }
 }

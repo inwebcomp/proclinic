9a355f9538c5bde2604b29d6c4897ac60387ce53
Escral <F9v6K4Fu42>
2019-07-19 10:32:35 +0300

    Service image

--- resources/views/service/extended-card.blade.php
+++ resources/views/service/extended-card.blade.php
@@ -2,7 +2,7 @@
     <div class="container article-card__container">
         <h4 class="article-card__title article-card__title--mobl">{{ $service->title }}</h4>
         <a href="{{ $service->path() }}" class="article-card__photo">
-            <img src="{{ optional($service->image)->getUrl('extended') }}" width="270" height=200" alt="{{ $service->title }}" class="article-card__img">
+            <img src="{{ optional($service->image)->getUrl('catalog') }}" width="270" height=200" alt="{{ $service->title }}" class="article-card__img">
         </a>
 
         <div class="article-card__content">

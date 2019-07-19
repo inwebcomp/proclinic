3a1dd3b44b97d55e8b15f200631cb9ed49dbb358
Escral <F9v6K4Fu42>
2019-07-19 11:28:31 +0300

    Hide articles if they are empty

--- resources/views/blocks/articles-slider.blade.php
+++ resources/views/blocks/articles-slider.blade.php
@@ -1,20 +1,22 @@
-<section class="block--articles-slider">
-    <div class="container">
-        <h3 class="block__title">@lang('Популярные записи')</h3>
+@if(count($articles))
+    <section class="block--articles-slider">
+        <div class="container">
+            <h3 class="block__title">@lang('Популярные записи')</h3>
 
-        <div class="articles-slider">
-            <div class="articles-slider__list js-slider">
-                @foreach ($articles as $article)
-                    @include('blog.article')
-                @endforeach
+            <div class="articles-slider">
+                <div class="articles-slider__list js-slider">
+                    @foreach ($articles as $article)
+                        @include('blog.article')
+                    @endforeach
+                </div>
+
+                <button class="slider-button slider-button--prev articles-slider__button--prev">
+                    <span class="icon icon--circle icon--fill icon--chevron"></span>
+                </button>
+                <button class="slider-button slider-button--next articles-slider__button--next">
+                    <span class="icon icon--circle icon--fill icon--chevron"></span>
+                </button>
             </div>
-
-            <button class="slider-button slider-button--prev articles-slider__button--prev">
-                <span class="icon icon--circle icon--fill icon--chevron"></span>
-            </button>
-            <button class="slider-button slider-button--next articles-slider__button--next">
-                <span class="icon icon--circle icon--fill icon--chevron"></span>
-            </button>
         </div>
-    </div>
-</section>
+    </section>
+@endif

9d3ab51a76d48f84ce49d25b78dc5fae169597d2
Escral <inwebcomp@gmail.com>
2019-09-23 09:52:19 +0300

    Admin config

---
+++ config/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+use InWeb\Admin\App\Http\Middleware\BootTools;
+use InWeb\Admin\App\Http\Middleware\DispatchServingAdminEvent;
+
+return [
+    'path' => 'admin',
+
+    'default_route' => '/',
+
+    'auth' => [
+        'login' => 'admin',
+        'email' => 'admin@sitename.com',
+        'password' => 'admin'
+    ],
+
+    'middleware' => [
+        'web',
+        DispatchServingAdminEvent::class,
+        BootTools::class,
+        'admin-auth',
+    ],
+];
